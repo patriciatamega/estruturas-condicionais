9c5ae0e6850b83567056b1c8f13959fcfd3105a4
Patrícia Tamega <45469119+patriciatamega@users.noreply.github.com>
2020-07-16 17:17:42 -0300

    Update exemplo-01.php

--- lacos-de-repeticao/for/exemplo-01.php
+++ lacos-de-repeticao/for/exemplo-01.php
@@ -1,6 +1,18 @@
 <?php
 
+/* for ($i = 0; $i <= 10; $i++) {
+
+    echo $i . "<br>"; */
+
+/* for ($i = 0; $i <= 1000; $i+=5) {
+
+    echo $i . "<br>"; */
+
 for ($i = 0; $i <= 10; $i++) {
+    
+    if ($1 > 200 && $i < 800) continue;
+    
+    if ($i === 900) break;
 
     echo $i . "<br>";
 
