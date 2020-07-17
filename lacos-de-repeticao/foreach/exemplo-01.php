ff41deeb6d64f4fe6845b1092af855adca2a3603
Patrícia Tamega <45469119+patriciatamega@users.noreply.github.com>
2020-07-16 21:42:57 -0300

    Aula 23 - Foreach

---
+++ lacos-de-repeticao/foreach/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+$meses = array(
+
+  "Janeiro", "Feveiro", "Março",
+  "Abril", "Maio", "Junho",
+  "Julho", "Agosto", "Setembro",
+  "Outobro", "Novembro", "Dezembro"
+
+);
+
+foreach ($meses as $index => $mes){
+
+  echo "Índice: ".$index."<br>";
+  echo "O mês é: ".$mes."<br><br>";
+
+}
+
+?>
